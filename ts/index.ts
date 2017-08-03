edcfe57ef0ba2950fa8fa2f42d0ed6253d859e8e
Artem Sakhatskiy <sakhatskiy@mindbox.ru>
2017-08-03 12:35:21 +0300

    minor bugfix

--- ts/index.ts
+++ ts/index.ts
@@ -1,38 +1,39 @@
 ﻿import * as React from 'react';
 
 declare global {
-	interface Window {
-		tr?: Function;
-		trl?: Function;
-		tre?: Function;
-	}
+    interface Window {
+        tml: {};
+        tr?: Function;
+        trl?: Function;
+        tre?: Function;
+    }
 }
 
 export function tr(...args: any[]): any { 
-	if (!window.tr || !args[0]) {
-		return args[0];
-	}
-	
-	return React.createElement("span",
-	{
-		dangerouslySetInnerHTML: {
-			__html: window.tr.apply(window, args)
-		}
-	});
+    if (!window.tr || !args[0]) {
+        return args[0];
+    }
+    
+    return React.createElement("span",
+    {
+        dangerouslySetInnerHTML: {
+            __html: window.tr.apply(window.tml, args)
+        }
+    });
 }
 
 export function trl(...args: any[]): any {
-	if (!window.trl || !args[0]) {
-		return args[0];
-	}
-	
-	return window.trl.apply(window, args);
+    if (!window.trl || !args[0]) {
+        return args[0];
+    }
+    
+    return window.trl.apply(window.tml, args);
 }
 
 export function tre(...args: any[]): any {
-	if (!window.tre || !args[0]) {
-		return args[0];
-	}
-	
-	return window.tre.apply(window, args);
+    if (!window.tre || !args[0]) {
+        return args[0];
+    }
+    
+    return window.tre.apply(window.tml, args);
 }
